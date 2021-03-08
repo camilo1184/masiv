866ca2480d931a5dc51e129cc7bc51c746b20d33
camilo_munoz <camilo1184@gmail.com>
2021-03-07 21:31:49 -0500

    initial commit

---
+++ src/main/java/co/com/masiv/MasivApplication.java
@@ -0,0 +1,13 @@
+package co.com.masiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/masiv/MasivApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.masiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
